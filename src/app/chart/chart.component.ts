1c13b7bb1e7db4a73da844619c34a6848718cf92
Roberto Dinca <roberto.dinca@key4talent.com>
2017-03-28 13:31:13 +0200

    Add chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+
+
+  }
 
   ngOnInit() {
+
+      var myChart = Highcharts.chart('container', {
+          chart: {
+              type: 'column'
+          },
+          plotOptions: {
+              column: {
+                  pointPadding: 0.3,
+                  borderWidth: 0
+              }
+          },
+          title: {
+              text: 'Dices Values Appearances '
+          },
+          xAxis: {
+              categories: ['Dice1', 'Dice2' , 'Dice3', 'Dice4']
+          },
+          yAxis: {
+              title: {
+                  text: 'Nr of appearances'
+              }
+          },
+          series: [{
+              name: '1',
+              data: [1, 0, 4, 0]
+          }, {
+              name: '2',
+              data: [5, 7, 3]
+          }, {
+              name: '3',
+              data: [4, 5, 1]
+          }, {
+              name: '4',
+              data: [4, 6, 1]
+          }, {
+              name: '5',
+              data: [2, 5, 5]
+          }, {
+              name: '6',
+              data: [7, 5, 6]
+          }
+
+
+          ]
+      });
+
   }
 
 }
